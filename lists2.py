0fb0012a53f289e6751b229acbddeb73db8b0dfb
minka69 <marks.sislovs@gmail.com>
2024-10-30 10:33:03 +0200

    simple list with python

---
+++ lists2.py
@@ -0,0 +1,16 @@
+names = ["anna", "oskars", "Jennifer"]
+
+#for name in names:
+#    print(f"Hey hey {name}!")
+
+ages = [16, 15, 21]
+University = [False, False, True]
+
+personal_data = [
+    ["Anna", 16, False],
+    ["Oskars", 15, False],
+    ["Jennifer", 21, True]
+]
+for data_list in personal_data:
+    for data in data_list:
+        print(data)
