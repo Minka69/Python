f8356de0a22d3ddf5a7d57a33437ee77724c70c3
minka69 <marks.sislovs@gmail.com>
2024-11-06 11:13:38 +0200

    Arrays test

---
+++ test.py
@@ -0,0 +1,14 @@
+import random
+from collections import Counter
+
+animals = ['cat', 'dog', 'sheep']
+weights = [0.5, 0.4, 0.1]
+
+selected_values = random.choices(animals, weights=weights, k=1000)
+
+summary = Counter(selected_values)
+for animal, count in summary.items():
+    percentage = (count / 1000) * 100
+    print(f"{animal}: {count} ({percentage:.2f}%)")
+
+
