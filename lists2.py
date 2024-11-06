c95a4d0104368cc2c9919af3d86c08426cdbaa90
minka69 <marks.sislovs@gmail.com>
2024-11-06 09:21:59 +0200

    Changes

--- lists2.py
+++ lists2.py
@@ -1,16 +1,22 @@
-names = ["anna", "oscar", "Jennifer"]
+names = ["Anna", "Oscar", "Jennifer", "Janis"]
 
 #for name in names:
 #    print(f"Hey hey {name}!")
 
-ages = [16, 15, 21]
-University = [False, False, True]
+ages = [16, 15, 21, 18]
+University = [False, False, True, True]
 
 personal_data = [
-    ["Anna", 16, False],
-    ["Oskars", 15, False],
-    ["Jennifer", 21, True]
+    ["Anna", 16, False, "anna@mail.com"],
+    ["Oskars", 15, False, "oscar@bestmail.com"],
+    ["Jennifer", 21, True, "jennifer@smitch.com"],
+    ["Janis", 18, True, "janis.krumins@gmail.com"]
 ]
-for data_list in personal_data:
-    for data in data_list:
-        print(data)
+
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+university = input('Are you in university? (yes/no): ').lower() == 'yes'
+email = input('Enter your email: ')
+
+new_data = [name, age, university, email]
+personal_data.append(new_data)
