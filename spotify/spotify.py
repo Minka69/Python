967f0ce860d6377611d51017e15b01ec437bb144
minka69 <marks.sislovs@gmail.com>
2024-12-04 08:37:54 +0000

    calculating duration of the songs

--- spotify/spotify.py
+++ spotify/spotify.py
@@ -3,7 +3,20 @@ import csv
 file_path = "/home/minka69btw/Code/Python/spotify/spotify.csv"
 
 with open(file_path, mode='r', newline='', encoding='utf-8') as file:
-    csv_reader = csv.reader(file)
-    
+    csv_reader = csv.reader(file)     
     for row in csv_reader:
-        print(row[0], "-", row[1], row[3], row[4], row[5])
+        print(f"{row[0]} - {row[1]}")
+
+with open(file_path, mode='r', newline='', encoding='utf-8') as file:
+    csv_reader = csv.reader(file)     
+    
+    total_duration = 0
+    track_count = 0
+    for row in csv_reader:
+        try:
+            total_duration += int(row[4])
+            track_count += 1
+        except:
+            continue
+    
+    print(f"Average song duration is: {(total_duration / track_count) / 1000} s")
