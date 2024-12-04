b7af637b2827647345ce4edfadfd10bdec42661b
minka69 <marks.sislovs@gmail.com>
2024-12-04 07:53:37 +0000

    minmor changes

--- spotify/spotify.py
+++ spotify/spotify.py
@@ -2,11 +2,8 @@ import csv
 
 file_path = "/home/minka69btw/Code/Python/spotify/spotify.csv"
 
-file = open(file_path, mode='r', newline='', encoding='utf-8')
-
-csv_reader = csv.reader(file)
-
-for row in csv_reader:
-    print(row)
-
-file.close()
+with open(file_path, mode='r', newline='', encoding='utf-8') as file:
+    csv_reader = csv.reader(file)
+    
+    for row in csv_reader:
+        print(row[0], "-", row[1], row[3], row[4], row[5])
